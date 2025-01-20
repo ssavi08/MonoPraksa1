58c411efc1d63ab001bf3cedeb0c44c8609b6858
Sandro Savi <108483809+ssavi08@users.noreply.github.com>
2025-01-20 14:55:08 +0100

    Update

--- ConsoleApp1/Interfaces/IPCStorage.cs
+++ ConsoleApp1/Interfaces/IPCStorage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,6 @@ namespace PCManagement.Interfaces
     public interface IPCStorage
     {
         void AddPC(PC pc);
-        //void EditPC(int id, Action<PC> editAction);
         void DeletePC(int id);
         IEnumerable<PC> GetAllPCs();
         PC? GetPCById(int id);
